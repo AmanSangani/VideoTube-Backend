cba71b62265091699c1a1637f34fea0b9e8ebe64
AmanSangani <127185092+AmanSangani@users.noreply.github.com>
2024-02-09 18:17:32 +0530

    multer

---
+++ src/utils/cloudService.js
@@ -0,0 +1,36 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: "amansangani",
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRETKEY,
+});
+
+const uploadOnCloud = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("File is Uploaded to Cloudinary : " + response.url);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath)
+    return null;
+  }
+};
+ 
+// cloudinary.uploader.upload(
+//   "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" },
+//   function (error, result) {
+//     console.log(result);
+//   }
+// );
+
+
+module.exports = {uploadOnCloud}
